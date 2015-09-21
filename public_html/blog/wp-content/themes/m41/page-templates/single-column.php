a082e1d009c587f7ff9e34aa5eb273dc45a12b8b
grundfuttock <geoff@m41.co.uk>
2015-09-21 15:15:12 +0100

    fixed responsive image display for post thumbnail Added admin user geoff Removed user admin TODO create gallery page template Add column shortcodes

--- public_html/blog/wp-content/themes/m41/page-templates/single-column.php
+++ public_html/blog/wp-content/themes/m41/page-templates/single-column.php
@@ -10,10 +10,10 @@
                         <?php
                         // check if the post or page has a Featured Image assigned to it.
                         if (has_post_thumbnail()) {
-                            $featured_image = get_the_post_thumbnail();
+                            $featured_image = get_the_post_thumbnail($post_id, 'post-thumbnail', array( 'class' => 'img-responsive' ));
                         }
                         ?>
-                            <img class="img-responsive" <?php echo ($featured_image); ?> >
+                        <?php echo ($featured_image); ?>
                              <h1 class="post-title"><?php the_title(); ?></h1>
                             <h2 class="subheading"><?php echo get_post_meta(get_the_id(), 'subtitle', true) ?></h2>
                     </div>
